929ead56ed8b06ce3941ea38c9f4582f7b2da44c
Ghanashyam Khanal <khanalg44@gmail.com>
2020-10-22 07:19:55 -0400

    new fixes

--- 03_ipl_consistency/print_scorecard.py
+++ 03_ipl_consistency/print_scorecard.py
@@ -7,6 +7,10 @@ import os, sys, yaml
 data_dir='../datasets/ipl/yaml/'
 
 def print_scorecard(f, data_dir='./'):
+
+    def add_player(player, scorecard):
+        if player not in scorecard:
+            scorecard[player] = {'0':0, '1':0, '2':0, '3':0, '4':0, '5':0, '6':0, 'R':0, 'B':0, 'NOT-OUT':True}
     
     fil=os.path.join(data_dir,  f )
     #data=yaml.load(open(fil))
@@ -17,15 +21,18 @@ def print_scorecard(f, data_dir='./'):
         except yaml.YAMLError as exc:
             print(exc)
 
+    try:
+        match_date=data['info']['dates'][0].strftime('%Y-%m-%d')
+    except:
+        match_date=data['info']['dates'][0]
+    year = match_date.split('-')[0]
 
     batting_card = {}    
     for inn in range(2):
         inn_name = list(data['innings'][inn].keys())[0]
 
         batting_card_inn={}
-        batting_card_inn[inn_name] = {'0':' ', '1':' ', '2':' ', '3':' ', '4':' ', '5':' ', '6':' ', 'R':' ', 'B':' '}
         
-        runs_bowler={}
         runs_extra = 0
         runs_total = 0
         wkts       = 0
@@ -40,12 +47,17 @@ def print_scorecard(f, data_dir='./'):
             runs_ext = deliv[1]['runs'].get('extras',  0)
             runs_tot = deliv[1]['runs'].get('total',   0)
             
-            if batsman not in batting_card_inn:
-                batting_card_inn[batsman] = {'0':0, '1':0, '2':0, '3':0, '4':0,  '5':0, '6':0, 'R':0, 'B':0}
+            add_player(batsman, batting_card_inn) 
 
-            batting_card_inn[batsman][str(runs_bat)] = batting_card_inn[batsman][str(runs_bat)]+1
+
+            # counter for each run (0, 1, 2, 3, 4, 5, 6)
+            batting_card_inn[batsman][str(runs_bat)] += 1  
+            
+            # counter for total batsman run
             batting_card_inn[batsman]['R'] += runs_bat
-            batting_card_inn[batsman]['B'] += 1 # remove the ball later if it's a wide
+            
+            # counter for toal balls faced [ball will be removed later if it's a wide]
+            batting_card_inn[batsman]['B'] += 1
             
             if 'extras' in deliv[1]:
                 if 'wides' in deliv[1]['extras']:
@@ -56,11 +68,16 @@ def print_scorecard(f, data_dir='./'):
             
             if deliv[1].get('wicket', None):
                 wkts += 1
+                player_out=deliv[1]['wicket']['player_out']
+
+                # for case when a player is run out without facing a ball
+                add_player(player_out, batting_card_inn)
+                batting_card_inn[player_out]['NOT-OUT']=False
 
         batting_card_inn['Total_inn'+str(inn+1)] = {'0':' ', '1':' ', '2':' ', '3':' ', '4':' ', '5':' ', '6':' ',
-                                                    'R':str(runs_total)+'-'+str(wkts), 'B':' '}
+                                                    'R':str(runs_total)+'-'+str(wkts), 'B':' ', 'NOT-OUT':' '}
         # empty row
-        batting_card_inn['  '] = {'0':' ', '1':' ', '2':' ', '3':' ', '4':' ', '5':' ', '6':' ', 'R':' ', 'B':' '}        
+        batting_card_inn['  '] = {'0':' ', '1':' ', '2':' ', '3':' ', '4':' ', '5':' ', '6':' ', 'R':' ', 'B':' ', 'NOT-OUT':' '}        
         batting_card.update(batting_card_inn)        
 
     
@@ -70,7 +87,7 @@ def print_scorecard(f, data_dir='./'):
     
     print ()
     print ("------------------------------- Match Info-------------------------------")
-    print (data['info']['teams'][0] ,' vs ', data['info']['teams'][1], ', ', data['info']['dates'][0].strftime('%Y-%m-%d'))
+    print (data['info']['teams'][0] ,' vs ', data['info']['teams'][1], ', ', match_date)
     print ( 'Toss \t\t\t', data['info']['toss']['winner'], 'Decided to', data['info']['toss']['decision'])
     print ('Result \t\t\t', data['info']['outcome']['winner'], 'won by ', 
            list(data['info']['outcome']['by'].values())[0], list(data['info']['outcome']['by'].keys())[0])
