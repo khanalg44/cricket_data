b48e192789e9322fc58ceeee2b987da2993e28fb
Ghanashyam Khanal <khanalg44@gmail.com>
2020-11-19 15:14:07 -0500

    small updates

--- 03_ipl/utils.py
+++ 03_ipl/utils.py
@@ -34,18 +34,26 @@ def add_overs(overs):
     for ov in overs:
         ov_split = ov.split('.')
         ov_int  += int(ov_split[0])
-        
+
         if len(ov_split)>1:
-            ov_frac += int(ov.split('.')[1])
-            
+            ov_frac += int(ov_split[1])
+
         if ov_frac >= 6:
             ov_int += ov_frac//6
             ov_frac = ov_frac %6
-            
+
     if ov_frac==0:
         return str(ov_int)
     else:
         return str(ov_int)+'.'+str(ov_frac)
+
+def Over2Balls(Over):
+    ov_split = Over.split('.')
+    all_balls = int(ov_split[0])*6
+
+    if len(ov_split) > 1:
+        all_balls += int( ov_split[1] )
+    return all_balls
 
 def get_player_profile(player, batsman=True):
     
@@ -53,13 +61,14 @@ def get_player_profile(player, batsman=True):
         df=pickle.load(open('./database/batting_record_all_years.df', 'rb'))
 
         data_player=[]
-        for season in range(2008, 2020, 1): 
+        for season in range(2008, 2021, 1): 
             dfp   = df[ (df['season']==str(season)) & (df['batsman']==player) ]
             Inns_ = dfp.shape[0]
 
             if Inns_ == 0:
-                data_player.append([season, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] )
+                data_player.append([season,' ', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] )
             else:
+                Team_ = dfp.Team.values[0]
                 Runs_ = dfp.Runs.sum()
                 BF_   = dfp.BF.sum()
                 NOs_  = int(dfp.NO.sum())
@@ -74,27 +83,36 @@ def get_player_profile(player, batsman=True):
                 Fifty_= ((dfp.Runs>=50) & (dfp.Runs<100) ).sum()
                 Hundred_= (dfp.Runs>=100).sum()
     
-                data_player.append([season, Inns_, NOs_, Runs_, BF_, HS_, Ave_, SR_, Fifty_, Hundred_, Fours_, Sixes_] )
-        df_p = pd.DataFrame(data_player, columns=['season', 'Innings', 'NO', 'Runs', 'BF', 'HS', 'Ave','SR', '50s', '100s', '4s', '6s'])
+                data_player.append([season, Team_, Inns_, NOs_, Runs_, BF_, HS_, Ave_, SR_, Fifty_, Hundred_, Fours_, Sixes_] )
+        df_p = pd.DataFrame(data_player, columns=['season', 'Team', 'Innings', 'NO', 'Runs', 'BF', 'HS', 'Ave','SR', '50s', '100s', '4s', '6s'])
         return df_p
 
     else:
         df=pickle.load(open('./database/bowling_record_all_years.df', 'rb'))
 
         data_player=[]
-        for season in range(2008, 2020, 1):
+        for season in range(2008, 2021, 1):
             dfp   = df[ (df['season']==str(season)) & (df['bowler']==player) ]
             Inns_ = dfp.shape[0]
 
             if Inns_ == 0:
-                data_player.append([season, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] )
+                data_player.append([season, ' ',0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] )
             else:
+                Team_ = dfp.Team.values[0]
                 Ovs_  = add_overs(dfp['O'].values)
                 Wkts_ = dfp['W'].sum()
-                
+                Runs_ = dfp['R'].sum()
+
                 #HS_   = max(dfp.Runs) to be calculated for best bowling figures
+                
+                Balls_ = Over2Balls(Ovs_)
+                
                 SR_   = 0.
                 Ave_  = 0.
+                if Wkts_ > 0:
+                    SR_   = round( Balls_ / Wkts_ , 2 )
+                    Ave_  = round( Runs_ / Wkts_  , 2 )
+
                 Fours_= dfp['4s'].sum()
                 Sixes_= dfp['6s'].sum()
                 WDs_  = dfp['WD'].sum()
@@ -105,22 +123,22 @@ def get_player_profile(player, batsman=True):
                 FourWs_ = ((dfp['W']>=4)).sum()
                 FiveWs_ = ((dfp['W']>=5)).sum()
                 
-                data_player.append([season, Inns_, Ovs_, Wkts_, SR_, Ave_, Fours_, Sixes_,
+                data_player.append([season, Team_, Inns_, Ovs_, Wkts_, SR_, Ave_, Fours_, Sixes_,
                                     WDs_, NBs_, NoWs_, ThreeWs_, FourWs_, FiveWs_])
 
-                        
-        df_p = pd.DataFrame(data_player, columns=['season', 'Innings', 'Overs', 'Wickets', 
+        df_p = pd.DataFrame(data_player, columns=['season', 'Team', 'Innings', 'Overs', 'Wickets', 
                                                   'SR', 'Ave', 'Fours', 'Sixes', 'WDs', 'NBs',
                                                   '0-Fers', '3-Fers', '4-Fers', '5-Fers'])                       
 
         return df_p
 
 if __name__=="__main__":
-    #player='NA Saini'
-    player='A Nehra'
-    dfp=get_player_profile(player, batsman=False)
-    print (dfp)
+    player='NA Saini'
+    #dfp=get_player_profile(player, batsman=False)
+    player='SK Raina'
+    player='MS Dhoni'
     dfp=get_player_profile(player, batsman=True)
+    #print (dfp)
     print (dfp)
     #df=get_match_list()
     #print ( df.head() )
